51a73e8783c75679acaff8beacdf566096c70958
Raul Jr <131274781+rauljraraujo2090@users.noreply.github.com>
2024-07-07 23:37:56 -0500

    sdsd

--- admin/src/operaciones/insertar.php
+++ admin/src/operaciones/insertar.php
@@ -7,11 +7,13 @@ $celular=$_POST['celular'];
 $correo=$_POST['correo'];
 $mensaje=$_POST['sms']; 
 
-
+ 
 $consulta="INSERT INTO   mensaje_contacto (nombre,apellidos,celular,correo,mensaje) VALUES ('$nombre','$apellidos','$celular','$correo','$mensaje')";
 
 $ejecutar=mysqli_query($con,$consulta );
  if($ejecutar){
+    include('index.php');
+    
     header("location:../../../contactos.php");
 
 
